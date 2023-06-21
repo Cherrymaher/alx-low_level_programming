fc1563f74261a338fd8fead55306e3ebb7fa1b73
Cherrymaher <7alarony@gmail.com>
2023-06-20 22:32:20 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@
  *
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
- *         -1 if the number is less than zero.
+ *        -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
